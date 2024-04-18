6a98207df56ac0eb60fd10081c95f588e4b53886
Lukas Wösenböck <WoesenboeckL21085@sus.htl-grieskirchen.at>
2024-04-18 08:28:08 +0200

    Added the project

---
+++ src/main/java/net/htlgkr/luwoes/receptarioAPI/ReceptarioApiApplication.java
@@ -0,0 +1,13 @@
+package net.htlgkr.luwoes.receptarioAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptarioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/htlgkr/luwoes/receptarioAPI/ReceptarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.htlgkr.luwoes.receptarioAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceptarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
